a5c944e387bf78621770c489298faf534da13450
Ivantech123 <_EMAIL__GITHUB>
2026-05-08 14:42:06 +0300

    build: clean npm build, fix swc bindings

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RTlLSkrnvLH59k3a485pW9qOK/ABEIeJZzJrbHymay8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
